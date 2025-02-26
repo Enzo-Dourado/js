12a1fe152ec0c076fd7d073c822dcbf20671da8f
Enzo Dourado <enzodourado.cripto@gmail.com>
2025-02-26 10:59:50 -0300

    Valores em Reais

---
+++ aula02/script_04.js
@@ -0,0 +1,3 @@
+var nome = prompt('Qual seu nome?')
+document.writeln(`O seu nome é ${nome.length}!`)
+document.writeln(`O seu nome em letras Maiúsculas é ${nome.toUpperCase()}, e em minúsculas é ${nome.toLowerCase()}!`)
