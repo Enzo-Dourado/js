f04138bf3f72f75e1a99875cb5ec6a7f314cfa1f
Enzo Dourado <enzodourado.cripto@gmail.com>
2025-03-06 17:34:10 -0300

    exercicios novos

---
+++ projetos-js/projeto-03 Contagem/script.js
@@ -0,0 +1,10 @@
+// Variáveis Iniciais
+function cliqueAqui() {
+    var inicio = document.getElementById('iinicio')
+    var fim = document.getElementById('ifim')
+    var passos = document.getElementById('ipassos')
+// Dados Salvos
+    var nInicio = Number(inicio.value)
+    var nFim = Number(fim.value)
+    
+}
